063850f6f65172d8cc5ce114c1cfeab1a8eada80
UnderDoug <douglas.marken@gmail.com>
2026-03-10 10:48:08 +1000

    removed redundant attributes from some cache fields.

--- Re-animatable Corpses/Startup.cs
+++ Re-animatable Corpses/Startup.cs
@@ -21,13 +21,11 @@ namespace UD_FleshGolems
         public static bool CachedCorpses = false;
 
         [GameBasedStaticCache(CreateInstance = false)]
-        [ModSensitiveStaticCache]
         public static string _PlayerBlueprint = null;
 
         public static string PlayerBlueprint => _PlayerBlueprint ??= Utils.GetPlayerBlueprint();
 
         [GameBasedStaticCache(CreateInstance = false)]
-        [ModSensitiveStaticCache]
         public static string _PlayerID = null;
 
         public static string PlayerID
@@ -46,55 +44,5 @@ namespace UD_FleshGolems
                 }
             }
         }
-
-        // Start-up calls in order that they happen.
-
-        [ModSensitiveCacheInit]
-        public static void ModSensitiveCacheInit()
-        {
-            // Called at game startup and whenever mod configuration changes
-        }
-
-        [GameBasedCacheInit]
-        public static void GameBasedCacheInit()
-        {
-            // Called once when world is first generated.
-
-            // The.Game registered events should go here.
-
-            UnityEngine.Debug.Log( nameof(Startup) + "." + nameof(GameBasedCacheInit) + ", " + nameof(PlayerBlueprint) + ": " + PlayerBlueprint ?? NULL);
-        }
-
-        // [PlayerMutator]
-
-        // The.Player.FireEvent("GameRestored");
-        // AfterGameLoadedEvent.Send(Return);  // Return is the game.
-
-        [CallAfterGameLoaded]
-        public static void OnLoadGameCallback()
-        {
-            // Gets called every time the game is loaded but not during generation
-
-            UnityEngine.Debug.Log(nameof(Startup) + "." + nameof(GameBasedCacheInit) + ", " + nameof(PlayerBlueprint) + ": " + PlayerBlueprint ?? NULL);
-        }
-
-        //
-        // End Startup calls
-        // 
     }
-
-    // [ModSensitiveCacheInit]
-
-    // [GameBasedCacheInit]
-
-    [PlayerMutator]
-    public class LearnAllTheBytes : IPlayerMutator
-    {
-        public void mutate(GameObject player)
-        {
-            // Called once when the player is generated (a fair bit after they're created.
-        }
-    }
-
-    // [CallAfterGameLoaded]
 }
