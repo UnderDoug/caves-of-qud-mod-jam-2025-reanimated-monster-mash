f3e6731be8a0f671c1311f8a9efbf1b568351615
UnderDoug <douglas.marken@gmail.com>
2026-03-10 10:39:13 +1000

    added missing part referenced in Creatures.xml.
    removed ProperNoun intProp from the mad monger.

---
+++ Re-animatable Corpses/Parts/UD_FleshGolems_DifficultyEvaluationAdjustment.cs
@@ -0,0 +1,39 @@
+﻿namespace XRL.World.Parts
+{
+    public class UD_FleshGolems_DifficultyEvaluationAdjustment : IScribedPart
+    {
+        public int AdjustRating;
+        public int MinRating;
+        public int MaxRating;
+
+        private bool WantAny
+            => AdjustRating != 0
+            || MinRating > int.MinValue
+            || MaxRating < int.MaxValue;
+
+        public UD_FleshGolems_DifficultyEvaluationAdjustment()
+        {
+            AdjustRating = 0;
+            MinRating = int.MinValue;
+            MaxRating = int.MaxValue;
+        }
+
+        public override bool WantEvent(int ID, int Cascade)
+            => base.WantEvent(ID, Cascade)
+            || (ID == GetDifficultyEvaluationEvent.ID && WantAny);
+
+        public override bool HandleEvent(GetDifficultyEvaluationEvent E)
+        {
+            if (AdjustRating != 0)
+                E.Rating += AdjustRating;
+
+            if (MinRating > int.MinValue)
+                E.MinimumRating(MinRating);
+
+            if (MaxRating < int.MaxValue)
+                E.MaximumRating(MaxRating);
+
+            return base.HandleEvent(E);
+        }
+    }
+}
